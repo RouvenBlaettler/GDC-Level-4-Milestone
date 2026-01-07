5376afa36d6504b4c8e87378ea241b5bcf9f3506
Rouven Blättler <rouvenblattler@gmail.com>
2026-01-07 23:41:51 +0100

    minor fix

--- tasks/views.py
+++ tasks/views.py
@@ -13,7 +13,7 @@ def task_view(request):
 
 def add_task_view(request):
     # If a `task` GET parameter is present, append and redirect.
-    task = request.GET.get('task', '')
+    task = request.GET['task']
     if task:
         tasks.append(task)
         return HttpResponseRedirect('/task')
@@ -21,9 +21,8 @@ def add_task_view(request):
     # Otherwise render the form and show current tasks
     return render(request, 'tasks.html', {'tasks': tasks})
 
-
 def delete_task_view(request, index):
     # Delete the task at the given index if it exists
-    if 1 <= index <= len(tasks):
+    if 1 <= index < len(tasks)+1:
         del tasks[index-1]
     return HttpResponseRedirect('/task')
