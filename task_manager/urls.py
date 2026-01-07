7d474c3a9c1130e8f2bda5a798cf7c4d5be71bd5
Rouven Blättler <rouvenblattler@gmail.com>
2026-01-07 23:40:24 +0100

    fix bug!!!

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-
+from tasks.views import task_view, add_task_view, delete_task_view
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("tasks/", task_view),
+    path('admin/', admin.site.urls),
+    path('task/', task_view),
+    path('', task_view),
+    path('task/add-task', add_task_view),
+    path('task/delete-task/<int:index>', delete_task_view)  # Home page mapped to task_view
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,1 +1,29 @@
-# Add all your views here
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+# Global in-memory storage for testing only (lost on server restart)
+tasks = []
+
+
+def task_view(request):
+    # Show current in-memory tasks
+    context = {'tasks': tasks}
+    return render(request, 'tasks.html', context)
+
+
+def add_task_view(request):
+    # If a `task` GET parameter is present, append and redirect.
+    task = request.GET.get('task', '')
+    if task:
+        tasks.append(task)
+        return HttpResponseRedirect('/task')
+
+    # Otherwise render the form and show current tasks
+    return render(request, 'tasks.html', {'tasks': tasks})
+
+
+def delete_task_view(request, index):
+    # Delete the task at the given index if it exists
+    if 1 <= index <= len(tasks):
+        del tasks[index-1]
+    return HttpResponseRedirect('/task')
